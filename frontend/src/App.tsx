da57b91f9e60dd1bf0717b07425a2774ff00fe1a
i243070-beep <i243070@isb.nu.edu.pk>
2026-09-26 22:14:42 +0500

    feat(frontend): add complaint statistics view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { AppShell } from "./components/AppShell";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { DashboardPage } from "./pages/DashboardPage";
+import { StatsPage } from "./pages/StatsPage";
 import { SubmitPage } from "./pages/SubmitPage";
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route element={<SubmitPage />} path="/submit" />
         <Route element={<DashboardPage />} path="/dashboard" />
+        <Route element={<StatsPage />} path="/stats" />
         <Route element={<Navigate replace to="/submit" />} path="*" />
       </Routes>
     </AppShell>
